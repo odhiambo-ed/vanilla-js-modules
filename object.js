ffb0f38335bdd558e4e8a09324c336182c3a60f8
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-21 12:56:21 +0300

    initial commit

---
+++ object.js
@@ -0,0 +1,11 @@
+const cars = {
+    name: "BMW",
+    type: "Sedan",
+    YOM: 2010
+}
+
+function myFunction() {
+    console.log("The name of the car is: " + cars.name + " , type is: " + cars.type + " and the year of Manufacture is: " + cars.YOM);
+}
+
+myFunction();
---
+++ variable.js
@@ -0,0 +1,5 @@
+let x = 5;
+let y = 10;
+//document.getElementById("demo").innerHTML = x + y;
+let z = x + y;
+alert(z)
